568acac5e24bb0fecc1018c51c4bef9f33dd8906
Wu Jian Ping <wujjpp@hotmail.com>
2019-05-08 18:00:49 +0800

    .

--- basic.py
+++ basic.py
@@ -41,8 +41,8 @@ for i in range(25):
     plt.subplot(5,5,i+1)
     plt.xticks([])
     plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i])
+    plt.imshow(train_images[i])    
+    plt.grid(True)
     # plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
 
