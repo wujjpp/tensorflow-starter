5c8e344318797e71fe5fc93562b2c851aee8204d
Wu Jian Ping <wujjpp@hotmail.com>
2019-05-08 15:59:17 +0800

    .

---
+++ save-3.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import tensorflow as tf
+from tensorflow import keras
+
+print('tf.version:' + str(tf.__version__))
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+train_labels = train_labels[:1000]
+test_labels = test_labels[:1000]
+
+train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
+# Returns a short sequential model
+def create_model():
+  model = tf.keras.models.Sequential([
+    keras.layers.Dense(512, activation=tf.nn.relu, input_shape=(784,)),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+  ])
+
+  model.compile(optimizer=tf.keras.optimizers.Adam(),
+                loss=tf.keras.losses.sparse_categorical_crossentropy,
+                metrics=['accuracy'])
+
+  return model
+
+# Create a basic model instance
+model = create_model()
+model.summary()
+
+model = create_model()
+model.fit(train_images, train_labels,
+          epochs = 50,
+          validation_data = (test_images,test_labels))
+
+# Save entire model to a HDF5 file
+model.save('my_model.h5')
