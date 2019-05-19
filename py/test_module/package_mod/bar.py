8ac0ad0a37c0b905c23c8ff2716d031a59993554
Wu Jian Ping <wujjpp@hotmail.com>
2019-05-19 20:52:45 +0800

    .

---
+++ py/test_module/package_mod/bar.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+def bar_func():
+    print("bar func called")
---
+++ py/test_module/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from package_mod.foo import foo_func
+from package_mod.bar import bar_func
+
+foo_func()
+bar_func()
